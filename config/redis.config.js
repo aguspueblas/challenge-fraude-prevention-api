4b53af03260829529821a46bf21325f79f8755be
aguspueblas <agustinpueblas@gmail.com>
2024-12-26 15:40:51 -0300

    Elimino comments

--- config/redis.config.js
+++ config/redis.config.js
@@ -1,20 +1,19 @@
 const configEnv = require("./env");
 let Redis;
 let redis;
-// Configuración de Redis, asumiendo que tienes Redis ejecutándose en un contenedor de Docker
 if (configEnv.SERVER.NODE_ENV === "test") {
   Redis = require("ioredis-mock");
   redis = new Redis({
-    host: process.env.REDIS_HOST || "localhost", // Cambiar por la IP del contenedor si usas Docker
-    port: process.env.REDIS_PORT || 6379, // Puerto por defecto de Redis
-    password: process.env.REDIS_PASSWORD || null, // Si tienes contraseña, añádela aquí
+    host: process.env.REDIS_HOST || "localhost", 
+    port: process.env.REDIS_PORT || 6379, 
+    password: process.env.REDIS_PASSWORD || null, // 
   });
 } else {
   Redis = require("ioredis");
   redis = new Redis({
-    host: process.env.REDIS_HOST || "localhost", // Cambiar por la IP del contenedor si usas Docker
-    port: process.env.REDIS_PORT || 6379, // Puerto por defecto de Redis
-    password: process.env.REDIS_PASSWORD || null, // Si tienes contraseña, añádela aquí
+    host: process.env.REDIS_HOST || "localhost",
+    port: process.env.REDIS_PORT || 6379, 
+    password: process.env.REDIS_PASSWORD || null,
   });
 }
 module.exports = redis;
